2d982b7da4ea3d82bbb6778ccb9773266541a802
john7319 <jonniekaras@gmail.com>
2024-04-03 18:37:13 +0300

    Added functions to filter, fetchurls and display the images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    fetch(imgUrl)
+    .then(success => success.json())
+    .then(data => {
+        const newDiv = document.getElementById("dog-image-container")
+        data.message.forEach(image => {
+            const newElement = document.createElement("img")
+            newElement.src = image
+            newDiv.appendChild(newElement)
+        });
+    });
+
+    const breedUrl = "https://dog.ceo/api/breeds/list/all";
+    fetch(breedUrl)
+    .then(get => get.json())
+    .then(data => {
+        const breedList = document.getElementById("dog-breeds");
+        const breedFilter = document.getElementById("breed-dropdown");
+      if (breedList && breedFilter) {
+        const breeds = data.message;
+        function filterBreeds(selectedLetter) {
+          const filteredBreeds = {};
+          for (const breed in breeds) {
+            if (breed.charAt(0).toLowerCase() === selectedLetter.toLowerCase()) {
+              filteredBreeds[breed] = breeds[breed];
+            }
+          }
+          return filteredBreeds;
+        }
+
+        function updateBreedList(filteredBreeds) {
+          breedList.innerHTML = ""; 
+          for (const breed in filteredBreeds) {
+            const listItem = document.createElement("li");
+            listItem.addEventListener("click", function () {
+                this.style.color = "blue";
+            });
+            listItem.textContent = breed;
+            breedList.appendChild(listItem);
+          }
+        }
+        updateBreedList(breeds);
+        breedFilter.addEventListener("change", function () {
+          const selectedLetter = this.value; 
+          if (selectedLetter === "all") {
+            updateBreedList(breeds);
+          } else {
+            const filteredBreeds = filterBreeds(selectedLetter);
+            updateBreedList(filteredBreeds);
+          }
+        });
+      }
+    })
+
+})
